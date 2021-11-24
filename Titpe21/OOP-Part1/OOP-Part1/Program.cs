867e02d01ff8aa427b3be5b3d946f761f9b5090d
Opilane TTHK <opilane@tthk.ee>
2021-11-24 13:03:32 +0200

    loomad

---
+++ Titpe21/OOP-Part1/OOP-Part1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace OOP_Part1
+{
+    class Program
+    {
+        class Dog
+        {
+            string name;
+            int spots;
+            double happiness;
+
+            public Dog(string _name, int _spots)
+            {
+                name = _name;
+                spots = _spots;
+                happiness = 0;
+                Console.WriteLine($"The dog {name} has been created.");
+            }
+            public string Name
+            {
+                get { return name; }
+            }
+            public int Spots
+            {
+                get { return spots; }
+            }
+            public double Happiness
+            {
+                get { return happiness; }
+            }
+            public void Barks()
+            {
+                Console.WriteLine("Woof-Woof!");
+                happiness += 0.2;
+            } 
+            public void Wags()
+            {
+                Console.WriteLine($"{Name}: Wiggle-Wiggle");
+
+            }
+
+            public void Rename(string NewName)
+            {
+                name = NewName;
+                /*Console.WriteLine($"Enter a new name for the dog {Mydog1.name}");*/
+            }
+             
+            public void ShwoDogData()
+            {
+                Console.WriteLine($"Name: {name}");
+                Console.WriteLine($"spots:{spots}");
+                Console.WriteLine($"level of happiness: {Happiness}");
+            }
+
+
+
+
+
+
+        }
+       
+        static void Main(string[] args)
+        {
+            Dog Mydog1 = new Dog("Pihkur", 10);
+            Console.WriteLine($"{Mydog1.Name}'s level of happiness: {Mydog1.Happiness}");
+            Dog Mydog2 = new Dog("Puhkur", 2);
+            Console.WriteLine($"{Mydog2.Name}'s level of happiness: {Mydog2.Happiness}");
+
+
+            while(Mydog1.Happiness != 1)
+            {
+                Mydog1.Barks();
+            }
+
+            Mydog1.Wags();
+
+            Console.WriteLine($"Enter a new name for the dog{Mydog1.Name}");
+            string userinput = Console.ReadLine();
+            Mydog1.Rename(userinput);
+            Mydog2.ShwoDogData();
+        }
+
+
+
+    }
+}
---
+++ Titpe21/OOP-Part1/OOPCat/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace OOP_Part1
+{
+    class Program
+    {
+        class Dog
+        {
+            string name;
+            int color;
+            double hungryness;
+
+            public Dog(string _name, int _color)
+            {
+                name = _name;
+                color = _color;
+                hungryness = 0;
+                Console.WriteLine($"The cat {name} has been created.");
+            }
+            public string Name
+            {
+                get { return name; }
+            }
+            public int Color
+            {
+                get { return Color; }
+            }
+            public double Hungryness
+            {
+                get { return hungryness; }
+            }
+            public void Barks()
+            {
+                Console.WriteLine("zzzzzzzzzz!");
+                hungryness += 0.2;
+            }
+            public void Wags()
+            {
+                Console.WriteLine($"{Name}: Says Meow");
+
+            }
+
+            public void Rename(string NewName)
+            {
+                name = NewName;
+                /*Console.WriteLine($"Enter a new name for the dog {Mydog1.name}");*/
+            }
+
+            public void ShwoDogData()
+            {
+                Console.WriteLine($"Name: {name}");
+                Console.WriteLine($"color:{color}");
+                Console.WriteLine($"level of hungryness: {Hungryness}");
+            }
+
+
+
+
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            Dog Mydog1 = new Dog("Pihkur", 10);
+            Console.WriteLine($"{Mydog1.Name}'s level of Hungryness: {Mydog1.Hungryness}");
+            Dog Mydog2 = new Dog("Puhkur", 2);
+            Console.WriteLine($"{Mydog2.Name}'s level of Hungryness: {Mydog2.Hungryness}");
+
+
+            while (Mydog1.Hungryness != 1)
+            {
+                Mydog1.Barks();
+            }
+
+            Mydog1.Wags();
+
+            Console.WriteLine($"Enter a new name for the cat{Mydog1.Name}");
+            string userinput = Console.ReadLine();
+            Mydog1.Rename(userinput);
+            Mydog2.ShwoDogData();
+        }
+
+
+
+    }
+}
